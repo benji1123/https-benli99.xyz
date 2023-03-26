3b7552673a6f7d69f7b34fb9c7794fc72ea077ec
Benjamin Li <benji112358@gmail.com>
2023-03-26 01:03:29 -0400

    change spotify icon to vinyl; spin when playing music; decrease icon size on desktop

--- source.js
+++ source.js
@@ -3,19 +3,27 @@
 // + music from here: https://www.youtube.com/watch?v=tnl-euH9mjI&ab_channel=LeslietheGoat
 
 let MUSIC_PLAYING = false;
-const DAYS_LIKE_THESE = 'media/audio/dayslikethese.mp3';
-const music = new Audio(DAYS_LIKE_THESE);
+const music = new Audio('media/audio/dayslikethese.mp3');
+const recordScratchFx1 = new Audio('media/audio/record-scratch.mp3');
+const recordScratchFx2 = new Audio('media/audio/record-scratch.mp3');
 
 function toggleMusic() {
     const groot = document.getElementById('groot')
+    const vinylIcon = document.getElementById('spotifyIcon')
     if (MUSIC_PLAYING) {
         // TODO: play a disc scratch sound fx
+        recordScratchFx2.play()
         music.pause();
         groot.style.animation="grootMoveDown 4s forwards";
+        vinylIcon.style.animation=""; // stop rotating icon
     } else {
         // TODO: play a disc scratch sound fx
-        music.play();
-        groot.style.animation="grootMoveUp 3s forwards";
+        recordScratchFx1.play();
+        recordScratchFx1.addEventListener("ended", () => {
+            music.play();
+            groot.style.animation="grootMoveUp 3s forwards";
+        });
+        vinylIcon.style.animation="rotate 8s infinite linear";
     }
     MUSIC_PLAYING = !MUSIC_PLAYING;
 }
