bb9a092f35c47b5a0e0618e48acf37584ce2a9a7
Benjamin Li <benji112358@gmail.com>
2023-03-27 23:35:56 -0400

    html: swap 500px for 002; shift art-credit up css: drop icon saturation music: randomize music & add city sounds

--- source.js
+++ source.js
@@ -3,27 +3,30 @@
 // + music from here: https://www.youtube.com/watch?v=tnl-euH9mjI&ab_channel=LeslietheGoat
 
 let MUSIC_PLAYING = false;
-const music = new Audio('media/audio/dayslikethese.mp3');
+
+const i = Math.floor(Math.random() * 2) + 1;
+const MUSIC = new Audio(`media/audio/${i}.mp3`);
+const citySounds = new Audio('media/audio/citySounds.mp3');
 const recordScratchFx1 = new Audio('media/audio/record-scratch.mp3');
 const recordScratchFx2 = new Audio('media/audio/record-scratch.mp3');
 
 function toggleMusic() {
     const groot = document.getElementById('groot')
-    const vinylIcon = document.getElementById('spotifyIcon')
+    const vinyl = document.getElementById('vinyl')
     if (MUSIC_PLAYING) {
         // TODO: play a disc scratch sound fx
         recordScratchFx2.play()
-        music.pause();
+        MUSIC.pause();
         groot.style.animation="grootMoveDown 4s forwards";
-        vinylIcon.style.animation=""; // stop rotating icon
+        vinyl.style.animation=""; // stop rotating icon
     } else {
-        // TODO: play a disc scratch sound fx
+        
         recordScratchFx1.play();
         recordScratchFx1.addEventListener("ended", () => {
-            music.play();
-            groot.style.animation="grootMoveUp 3s forwards";
+            MUSIC.play();
+            citySounds.play();
+            groot.style.animation="grootMoveUp 5s forwards";
         });
-        vinylIcon.style.animation="rotate 8s infinite linear";
     }
     MUSIC_PLAYING = !MUSIC_PLAYING;
 }
