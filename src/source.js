668642e09350e6c7ce627dbb840ed55a7eafba39
Benjamin Li <benji112358@gmail.com>
2023-04-06 16:18:35 -0400

    change font to Futura; put icons in center instead of right

--- src/source.js
+++ src/source.js
@@ -6,34 +6,18 @@ const loadAnimatedBackground = async () => {
     })
     console.log(`fetched image: ${ANIMATED_BACKGROUND}`);
     const bgrndURL = imageBlob.url;
-    const left = document.getElementById('left');
-    left.style.backgroundImage = `url(${bgrndURL})`;
+    const cyber = document.getElementById('cyber-container');
+    cyber.style.backgroundImage = `url(${bgrndURL})`;
     URL.revokeObjectURL(bgrndURL);
 }
 
-let MUSIC_PLAYING = false;
 const MUSIC = new Audio("https://media.githubusercontent.com/media/benji1123/https-benli99.xyz/master/media/audio/4.mp3");
-const citySounds = new Audio('https://media.githubusercontent.com/media/benji1123/https-benli99.xyz/master/media/audio/citySounds.mp3');
-const recordScratchFx1 = new Audio('https://media.githubusercontent.com/media/benji1123/https-benli99.xyz/master/media/audio/record-scratch.mp3');
-const recordScratchFx2 = new Audio('https://media.githubusercontent.com/media/benji1123/https-benli99.xyz/master/media/audio/record-scratch.mp3');
+MUSIC.volume = 0.1
 
 const toggleMusic = () => {
-    const groot = document.getElementById('groot')
-    const vinyl = document.getElementById('vinyl')
-    if (MUSIC_PLAYING) {
-        recordScratchFx2.play()
-        MUSIC.pause();
-        groot.style.animation="grootMoveDown 2s forwards";
-        // stop rotating icon
-        vinyl.style.animation="";
+    if(MUSIC.paused) {
+        MUSIC.play()
     } else {
-        
-        recordScratchFx1.play();
-        recordScratchFx1.addEventListener("ended", () => {
-            MUSIC.play();
-            citySounds.play();
-            groot.style.animation="grootMoveUp 5s forwards";
-        });
+        MUSIC.pause()
     }
-    MUSIC_PLAYING = !MUSIC_PLAYING;
 }
