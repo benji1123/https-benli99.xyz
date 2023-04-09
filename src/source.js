df16cc457e63001852eee1c79b531a30a0a69272
Benjamin Li <benji112358@gmail.com>
2023-04-09 15:56:42 -0400

    move audioCtx creation to onClick
    Was getting error: The AudioContext was not allowed to start. It must be resumed (or created) after a user gesture on the page. https://goo.gl/7K7WLu

--- src/source.js
+++ src/source.js
@@ -15,12 +15,14 @@ const API_GET_SONG = 'https://pshlsoawx7.execute-api.us-east-1.amazonaws.com/pro
 var audioContext = new (window.AudioContext || window.webkitAudioContext)();
 console.log(audioContext.destination)
 
-const audioCtx = new AudioContext()
-const playSound = audioCtx.createBufferSource();
+let audioCtx
+let playSound
 let MUSIC_PLAYING = true
 let MUSIC_STARTED = false
 
 const getSongFromAPI = () => {
+    audioCtx = new AudioContext()
+    playSound = audioCtx.createBufferSource()
     // TODO: get this song from API instead of local storage
     const song_1980_dont_stop = '../media/audio/heart-change-brock-hewitt-stories-in-sound-main-version-04-28-13106.mp3'
     fetch(song_1980_dont_stop)
