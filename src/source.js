d762739dfa5f482dd581ce19557ace6e29f400bf
Benjamin Li <benji112358@gmail.com>
2023-04-25 22:30:03 -0400

    jeff bezos

--- src/source.js
+++ src/source.js
@@ -12,38 +12,52 @@ const lazyLoadAnimatedBackgroundFile = async () => {
     URL.revokeObjectURL(bgrndURL);
 }
 
-const API_GET_SONG = 'https://pshlsoawx7.execute-api.us-east-1.amazonaws.com/prod'
+let MUSIC_PLAYING = false;
+const NUM_SONGS = 4;
 
-let audioCtx
-let playSound
-let MUSIC_PLAYING = true
-let MUSIC_STARTED = false
+const MUSIC = new Audio(`media/audio/jeffrey-bezos.mp3`);
 
-const getSongFromAPI = () => {
-    audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    playSound = audioCtx.createBufferSource()
-    // TODO: get this song from API instead of local storage
-    const song_1980_dont_stop = '../media/audio/heart-change-brock-hewitt-stories-in-sound-main-version-04-28-13106.mp3'
-    fetch(song_1980_dont_stop)
-        .then(data => data.arrayBuffer())
-        .then(buf => audioCtx.decodeAudioData(buf))
-        .then(decodedAudio => {
-            playSound.buffer = decodedAudio;
-            playSound.connect(audioCtx.destination);
-            playSound.start(audioCtx.currentTime);
-            console.log(audioCtx);
-        });
-}
-
-const toggleMusic = () => {
-    if (!MUSIC_STARTED) {
-        MUSIC_STARTED = true
-        getSongFromAPI()
-        return
+function toggleMusic() {
+    if (MUSIC_PLAYING) {
+        MUSIC.pause();
+    } else {
+        MUSIC.play();
     }
-    if(MUSIC_PLAYING)
-        audioCtx.suspend()
-    else
-        audioCtx.resume()
-    MUSIC_PLAYING = !MUSIC_PLAYING
+    MUSIC_PLAYING = !MUSIC_PLAYING;
 }
+
+// const API_GET_SONG = 'https://pshlsoawx7.execute-api.us-east-1.amazonaws.com/prod'
+
+// let audioCtx
+// let playSound
+// let MUSIC_PLAYING = true
+// let MUSIC_STARTED = false
+
+// const getSongFromAPI = () => {
+//     audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+//     playSound = audioCtx.createBufferSource()
+//     // TODO: get this song from API instead of local storage
+//     const song_1980_dont_stop = '../media/audio/heart-change-brock-hewitt-stories-in-sound-main-version-04-28-13106.mp3'
+//     fetch(song_1980_dont_stop)
+//         .then(data => data.arrayBuffer())
+//         .then(buf => audioCtx.decodeAudioData(buf))
+//         .then(decodedAudio => {
+//             playSound.buffer = decodedAudio;
+//             playSound.connect(audioCtx.destination);
+//             playSound.start(audioCtx.currentTime);
+//             console.log(audioCtx);
+//         });
+// }
+
+// const toggleMusic = () => {
+//     if (!MUSIC_STARTED) {
+//         MUSIC_STARTED = true
+//         getSongFromAPI()
+//         return
+//     }
+//     if(MUSIC_PLAYING)
+//         audioCtx.suspend()
+//     else
+//         audioCtx.resume()
+//     MUSIC_PLAYING = !MUSIC_PLAYING
+// }
